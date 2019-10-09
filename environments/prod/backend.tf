fb9fe05522492bfc6af86a41123345d348d728a1
Rodney <hardworker30@gmail.com>
2019-10-09 17:41:58 -0400

    updated information

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "build-demo-251418-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="build-demo-251418"
